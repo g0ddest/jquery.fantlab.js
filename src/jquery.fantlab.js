5604dbed8d4640b4f99cd74474d78320b68411ed
Alexey Lvov <crealist@mail.ru>
2015-03-26 09:31:09 +0300

    оформление3

--- src/jquery.fantlab.js
+++ src/jquery.fantlab.js
@@ -8,8 +8,9 @@
 		async: true,
 		param_type: "data-fantlab_type", // "data- only on html5"
 		param_id: "data-fantlab_id",
-		innercss: "box-shadow: 1px 1px 2px rgba(0,0,0,0.2);font-family: Verdana, Arial; line-height: 1.2; cursor: default; vertical-align: top; position: absolute; padding: 10px; width: 230px; border-width: 1px 4px 1px 1px; border-style: solid; border-color: #adb4b8 #0079ab #adb4b8 #adb4b8; color: rgb(0, 0, 0); text-align: left; top: 27px; right: -200px; z-index: 999; outline: none; display: none; background-color: rgb(249, 250, 251);",
-        corner: "<span style='display: block!important; box-shadow: -1px 1px 0px rgba(0,0,0,0.2);width: 12px;height: 12px; position: absolute;margin-left: 20px;margin-top: -16px;background-color: rgb(249,250,251);z-index: 980;-webkit-transform: rotate(135deg);transform: rotate(135deg);'></span>"
+		innercss_work: "box-shadow: 1px 1px 2px rgba(0,0,0,0.2);font-family: Verdana, Arial; line-height: 1.2; cursor: default; vertical-align: top; position: absolute; padding: 10px; width: 230px; border-width: 1px 4px 1px 1px; border-style: solid; border-color: #adb4b8 #0079ab #adb4b8 #adb4b8; color: rgb(0, 0, 0); text-align: left; top: 27px; right: -200px; z-index: 999; outline: none; display: none; background-color: rgb(249, 250, 251);",
+		innercss_edition: "box-shadow: 1px 1px 2px rgba(0,0,0,0.2);font-family: Verdana, Arial; line-height: 1.2; cursor: default; vertical-align: top; position: absolute; padding: 10px; width: 230px; border-width: 1px 4px 1px 1px; border-style: solid; border-color: #adb4b8 #C45E24 #adb4b8 #adb4b8; color: rgb(0, 0, 0); text-align: left; top: 27px; right: -200px; z-index: 999; outline: none; display: none; background-color: rgb(249, 250, 251);",
+                corner: "<span style='display: block!important; box-shadow: -1px 1px 0px rgba(0,0,0,0.2);width: 12px;height: 12px; position: absolute;margin-left: 20px;margin-top: -16px;background-color: rgb(249,250,251);z-index: 980;-webkit-transform: rotate(135deg);transform: rotate(135deg);'></span>"
 	};
     var options;
     $.fn.fantlab = function(params){
@@ -77,15 +78,22 @@
                                 autors = autors.join(', ');
 
                                 var imgsmall = this.imgsmall ? this.imgsmall[0] : "//data.fantlab.ru/images/editions/small/0";
-                                var rusname = (this.rusname && this.rusname[0]) ? this.rusname[0] + (this.origname[0] ? "</a> / <a style='font-size: 12px;font-weight: bold' href='" + options.host + "work" + this.id + "'>" + this.origname[0] : '') : this.origname[0];
-                                var year = this.year[0];
+                                var rusname = (this.rusname && this.rusname[0]) ? 
+                                    "<a style='font-size: 12px;font-weight: bold' href='" + options.host + "work" + this.id + "'>" + this.rusname[0] + "</a>" +
+                                    (this.origname[0] ? "<b> / " + this.origname[0] + "</b>" : '') : "<a style='font-size: 12px;font-weight: bold' href='" + options.host + "work" + this.id + "'>" + this.origname[0] + "</a>";
+//                                var year = this.year[0];
                                 var rating = this.rating ? this.rating[0].content : 0;
                                 var voters = this.rating ? this.rating[0].voters : 0;
+                                var type_year = this.worktype[0]?this.worktype[0]:'';
+                                type_year += type_year?', ':'';
+                                type_year += this.year[0]?this.year[0]+' г.':'';
+                                if (type_year) { type_year = ' (' + type_year + ')' }
+               
 //"<a style='border: 0;text-decoration:none;' href='" + options.host + "work" + this.id + "'><img style='border: 0;float:left; padding-right: 10px;' src='" + imgsmall + "'></a>" + 
                                 var text = options.corner + 
-"<p style='vertical-align: top;margin: 0;text-indent:0;margin-bottom:3px'>" + autors + "</p>" + 
-"<p style='vertical-align: top;margin: 0;text-indent:0;margin-bottom:8px'><a style='font-size: 12px;font-weight: bold' href='" + options.host + "work" + this.id + "'>" + rusname + "</a>" + (year>0?" (" + year + " г.)":"") + "</p>" +
-"<p style='vertical-align: top;margin: 0;text-indent:0;margin-bottom:3px'>Рейтинг: " + (voters>0 ? rating + " (" + voters + ")" : "-") + "</p>";
+"<p style='vertical-align: top;margin: 0;text-indent:0;margin-bottom:5px'>" + autors + "</p>" + 
+"<p style='vertical-align: top;margin: 0;text-indent:0;margin-bottom:8px'>" + rusname + type_year + "</p>" +
+"<p style='vertical-align: top;margin: 0;text-indent:0;margin-bottom:3px'>Рейтинг: " + (voters>0 ? "<b>"+rating+"</b>" + " (" + voters + ")" : "-") + "</p>";
 
                                 var self = this;
 
@@ -94,12 +102,12 @@
                                     if(options.params_in_class){
 
                                         if($(this).hasClass("work_" + self.id)){
-                                            $(this).append('<div style="'+options.innercss+'">'+text+'</div>');
+                                            $(this).append('<div style="'+options.innercss_work+'">'+text+'</div>');
                                         }
                                     }else{
                                         if($(this).attr(options.param_type) === "work" && $(this).attr(options.param_id) === self.id){
 
-                                            $(this).append('<div style="'+options.innercss+'">'+text+'</div>');
+                                            $(this).append('<div style="'+options.innercss_work+'">'+text+'</div>');
                                             $(this).focus(function(){
                                                 this.blur();
                                             });
@@ -164,12 +172,12 @@
                                     if(options.params_in_class){
 
                                         if($(this).hasClass("edition_" + self.id)){
-                                            $(this).append('<div style="'+options.innercss+'">'+text+'</div>');
+                                            $(this).append('<div style="'+options.innercss_edition+'">'+text+'</div>');
                                         }
                                     }else{
                                         if($(this).attr(options.param_type) === "edition" && $(this).attr(options.param_id) === self.id){
 
-                                            $(this).append('<div style="'+options.innercss+'">'+text+'</div>');
+                                            $(this).append('<div style="'+options.innercss_edition+'">'+text+'</div>');
                                             $(this).focus(function(){
                                                 this.blur();
                                             });
